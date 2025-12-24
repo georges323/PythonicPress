841a833341a416afcc58351be919a1bf863bd8ca
georges <georges19232@gmail.com>
2025-12-24 15:37:19 -0800

    added extraction regex for links and images

--- src/test_markdown_parser.py
+++ src/test_markdown_parser.py
@@ -1,7 +1,7 @@
 import unittest
 
 from htmlnode import ParentNode, LeafNode
-from markdown_parser import split_nodes_delimiter
+from markdown_parser import extract_markdown_images, extract_markdown_links, split_nodes_delimiter
 from textnode import TextNode, TextType
 
 class TestMarkdownParser(unittest.TestCase):
@@ -50,6 +50,20 @@ class TestMarkdownParser(unittest.TestCase):
         ]
 
         self.assertEqual(new_nodes, expected_new_nodes)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://i.imgur.com/zjjcJKZ.png) and this [other link](https://www.google.com)"
+        )
+
+        self.assertListEqual([("link", "https://i.imgur.com/zjjcJKZ.png"), ("other link", "https://www.google.com")], matches)
 
 if __name__ == "__main__":
     unittest.main()
