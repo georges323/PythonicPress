241e62cf8f34323765334a361cf77d6b8fdf4a50
georges <georges19232@gmail.com>
2025-12-25 13:49:30 -0800

    cleaned it up more

--- src/markdown_page_parser.py
+++ src/markdown_page_parser.py
@@ -11,54 +11,73 @@ def markdown_to_html_node(markdown):
   children_nodes = []
   
   for block in blocks:
-    block_type = block_to_block_type(block)
-
-    nodes = []
-    match block_type:
-      case BlockType.CODE:
-        lines = block.split('\n')
-
-        code_node = TextNode(f'{"\n".join(lines[1:-1])}\n', TextType.CODE_TEXT)
-
-        children_nodes.append(ParentNode('pre', [text_node_to_html_node(code_node)]))
-        continue
-      case BlockType.HEADING:
-        match = re.match(r"^(#{1,6}) (.*)", block)
-
-        if match is None:
-          raise Exception("Heading has incorrect syntax")
-
-        nodes = text_to_children_nodes(match.group(2))
-        tag = f'h{len(match.group(1))}'
-      case BlockType.PARAGRAPH:
-        nodes = text_to_children_nodes(block.replace('\n', ' '))
-        tag = 'p'
-      case BlockType.QUOTE:
-        lines = block.split('\n')
-
-        quote = lines[0][2:]
-        for i in range(1, len(lines)):
-          quote += lines[i][1:]
-
-        nodes = text_to_children_nodes(quote)
-        tag = 'blockquote'
-      case BlockType.UNORDERED_LIST:
-        lines = block.split('\n')
-
-        for line in lines:
-          nodes.append(ParentNode('li', text_to_children_nodes(line[2:])))
-        tag = 'ul'
-      case BlockType.ORDERED_LIST:
-        lines = block.split('\n')
-
-        for line in lines:
-          nodes.append(ParentNode('li', text_to_children_nodes(line.split(".")[1][1:])))
-
-        tag = 'ol'
-
-    children_nodes.append(ParentNode(tag, nodes))
+    block_html_node = block_to_html_node(block)
+    children_nodes.append(block_html_node)
 
   return ParentNode('div', children_nodes)
+
+def block_to_html_node(block):
+  block_type = block_to_block_type(block)
+  match block_type:
+    case BlockType.CODE:
+      return code_block_to_html_node(block)
+    case BlockType.HEADING:
+      return heading_block_to_html_node(block)
+    case BlockType.PARAGRAPH:
+      return paragraph_block_to_html_node(block)
+    case BlockType.QUOTE:
+      return quote_block_to_html_node(block)
+    case BlockType.UNORDERED_LIST:
+      return unordered_list_to_html_node(block)
+    case BlockType.ORDERED_LIST:
+      return ordered_list_to_html_node(block)
+
+def code_block_to_html_node(block):
+  lines = block.split('\n')
+
+  code_text_node = TextNode(f'{"\n".join(lines[1:-1])}\n', TextType.CODE_TEXT)
+  child_node = text_node_to_html_node(code_text_node)
+
+  return ParentNode('pre', [child_node])
+
+def heading_block_to_html_node(block):
+  match = re.match(r"^(#{1,6}) (.*)", block)
+
+  if match is None:
+    raise Exception("Heading has incorrect syntax")
+
+  return ParentNode(f'h{len(match.group(1))}', text_to_children_nodes(match.group(2)))
+
+def paragraph_block_to_html_node(block):
+  children = text_to_children_nodes(block.replace('\n', ' '))
+  return ParentNode('p', children)
+
+def quote_block_to_html_node(block):
+  lines = block.split('\n')
+
+  quote = lines[0][2:]
+  for i in range(1, len(lines)):
+    quote += lines[i][1:]
+
+  return ParentNode('blockquote', text_to_children_nodes(quote))
+
+def unordered_list_to_html_node(block):
+  lines = block.split('\n')
+
+  children= []
+  for line in lines:
+    children.append(ParentNode('li', text_to_children_nodes(line[2:])))
+
+  return ParentNode('ul', children)
+
+def ordered_list_to_html_node(block):
+  lines = block.split('\n')
+
+  children= []
+  for line in lines:
+    children.append(ParentNode('li', text_to_children_nodes(line.split(".")[1][1:])))
+
+  return ParentNode('ol', children)
 
 def text_to_children_nodes(text):
   text_nodes = text_to_textnodes(text)
